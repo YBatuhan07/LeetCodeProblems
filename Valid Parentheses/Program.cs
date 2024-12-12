c9ff4c26b987a94639257e0d4dd43913878baf09
Batuhan Keskin <bkeskin394@gmail.com>
2024-12-12 12:57:51 +0300

    Let Code Problems

---
+++ Valid Parentheses/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Valid_Parentheses
+{
+    internal class Program
+    {
+
+        public static bool IsValid(string s)
+        {
+            while (s.Contains("()") || s.Contains("{}") || s.Contains("[]"))
+            {
+                s= s.Replace("()", "").Replace("{}", "").Replace("[]", "");
+            }
+            return s.Length == 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("true: "+Program.IsValid("()"));
+            Console.WriteLine("true: "+Program.IsValid("()[]{}"));
+            Console.WriteLine("false: "+Program.IsValid("(]"));
+            Console.WriteLine("true: "+Program.IsValid("([])"));
+        }
+    }
+}
