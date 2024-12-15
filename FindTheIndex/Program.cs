5669c40eccdd7cb2118bdce8b80823ca5d0b2f0b
Batuhan Keskin <bkeskin394@gmail.com>
2024-12-15 16:07:10 +0300

    LetCode
    RemoveElement,FındTheIndex

---
+++ FindTheIndex/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace FindTheIndex
+{
+    internal class Program
+    {
+        //public static int StrStr(string haystack, string needle)
+        //{
+        //    return haystack.IndexOf(needle);
+        //}
+
+        public static int StrStr(string haystack, string needle)
+        {
+            for (int i = 0; i <= haystack.Length - needle.Length; ++i)
+            {
+                if (haystack.Substring(i, needle.Length) == needle)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("0 ," + Program.StrStr("sadbutsad", "sad"));
+            Console.WriteLine("-1 ," + Program.StrStr("leetcode", "leeto"));
+            Console.WriteLine("4, " + Program.StrStr("leetcode", "code"));
+            Console.WriteLine("3, " + Program.StrStr("sadbutsad", "but"));
+            
+
+
+
+        }
+
+    }
+}
---
+++ RemoveElement/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace RemoveElement;
+
+internal class Program
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int res = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[res] = nums[i];
+                res++;
+            }
+        }
+        return res;
+    }
+
+    static void Main(string[] args)
+    {
+        Program p1 = new();
+
+        Console.WriteLine("1," + p1.RemoveElement([1, 2, 2], 2));
+        Console.WriteLine("7," + p1.RemoveElement([1, 4, 2, 5, 1,2,1,2], 5));
+        Console.WriteLine("1," + p1.RemoveElement([3, 3, 3, 1], 3));
+        Console.WriteLine("4," + p1.RemoveElement([1, 2, 3, 4, 5], 1));
+
+    }
+}
