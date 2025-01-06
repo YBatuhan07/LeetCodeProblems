96982cfa4475fd47739080caef251230d9463f1b
Batuhan Keskin <bkeskin394@gmail.com>
2025-01-06 20:26:32 +0300

    Pascal's Triangle

--- AddBinary/obj/Debug/net8.0/AddBinary.AssemblyInfo.cs
+++ AddBinary/obj/Debug/net8.0/AddBinary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AddBinary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07371d5849bfd1e9438bdd5d2e67b31d275780c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb330940cc26d64d4c0d79908267ecc1452ebf3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AddBinary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AddBinary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Sqrt/obj/Debug/net8.0/Sqrt.AssemblyInfo.cs
+++ Sqrt/obj/Debug/net8.0/Sqrt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sqrt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07371d5849bfd1e9438bdd5d2e67b31d275780c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb330940cc26d64d4c0d79908267ecc1452ebf3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sqrt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sqrt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
