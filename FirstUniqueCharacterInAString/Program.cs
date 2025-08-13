b4dd0f0e094b70ae2aeeabbebff20448f8c713de
Batuhan Keskin <bkeskin394@gmail.com>
2025-08-13 23:57:42 +0300

    First Unique Character in a String

---
+++ FirstUniqueCharacterInAString/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace FirstUniqueCharacterInAString;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        string test1 = "batuhan";
+        string test2 = "abcdabc";
+        string test3 = "aabb";
+
+        Console.WriteLine(FirstUniqChar(test1));
+        Console.WriteLine(FirstUniqChar(test2));
+        Console.WriteLine(FirstUniqChar(test3));
+    }
+
+    public static int FirstUniqChar(string s)
+    {
+        var charCount = new Dictionary<char, int>();
+
+        foreach (var c in s)
+        {
+            charCount[c] = charCount.GetValueOrDefault(c) + 1;
+        }
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (charCount[s[i]] == 1)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
